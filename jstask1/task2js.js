58e0c9f3035ab2320c9aa09fc6e4e2afebde9d10
tharun20070912 <tharunsiva2007@gmail.com>
2025-09-26 14:31:19 +0530

    n th commit :(

--- jstask1/task2js.js
+++ jstask1/task2js.js
@@ -19,36 +19,32 @@ while (a === true) {
  
 //prgm2
  
- let username;
- let psd;
- let cnfm;
- var choice;
- username=prompt("Please enter your username");
- psd=prompt("Please enter the password");
- if (username=="library" && psd=="Book123"){
-     cnfm=confirm("Do you want to borrow a book?");
-         if(cnfm==true){
-             choice=prompt("1=Fiction, 2=Science, 3=History");
-             switch(choice){
-                 case 1:
-                     alert("You have selected Fiction");
-                     break;
-                 case 2:
-                     alert("You have selected Science");
-                     break;
-                 case 3:
-                     alert("you have selected History");
-                     break;
-                 default:
-                     alert("Invalid catagory!");        
-             }
-         }
-             else {
-                 alert("Maybe next time! Goodbye!");
-             }
-     }          
- else{ 
-     alert("Invalid login!");
- }
-
-
+let username;
+let psd;
+let cnfm;
+let choice;
+username = prompt("Please enter your username");
+psd = prompt("Please enter the password");
+if (username == "library" && psd == "book123") {
+    cnfm = confirm("Do you want to borrow a book?");
+    if (cnfm == true) {
+        choice = prompt("1=Fiction, 2=Science, 3=History");
+        switch (choice) {
+            case "1":
+                alert("You have selected Fiction");
+                break;
+            case "2":
+                alert("You have selected Science");
+                break;
+            case "3":
+                alert("You have selected History");
+                break;
+            default:
+                alert("Invalid category!");
+        }
+    } else {
+        alert("Maybe next time! Goodbye!");
+    }
+} else {
+    alert("Invalid login!");
+}
